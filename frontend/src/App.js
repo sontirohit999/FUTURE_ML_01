9d3ff99a6977084e59e1cb5dc7f13768fadc390b
Sonti Rohit <sontirohit999@gmail.com>
2026-05-15 12:59:32 +0530

    Updated frontend API URL

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
     setAnimateResult(false);
 
     try {
-      const response = await fetch('http://127.0.0.1:5000/predict', {
+      const response = await fetch('https://future-ml-01-01.onrender.com//predict', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ date })
